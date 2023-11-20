b659e0da704a297a67c6f4f30dd13560624e3def
Marwan <maroelsays2013@gmail.com>
2023-11-20 19:48:23 +0200

    added project backend starter folders

---
+++ CentralStudentHub/src/main/java/com/centralstudenthub/CentralStudentHub/CentralStudentHubApplication.java
@@ -0,0 +1,13 @@
+package com.centralstudenthub.CentralStudentHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralStudentHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralStudentHubApplication.class, args);
+	}
+
+}
