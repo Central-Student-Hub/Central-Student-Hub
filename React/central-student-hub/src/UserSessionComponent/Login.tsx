c85f4fb92448ed4a73a02666fef34ae1c17c6ae4
mohamed adel <muhammedadeltaha@gmail.com>
2023-11-27 23:38:53 +0200

    Update the style

--- React/central-student-hub/src/UserSessionComponent/Login.tsx
+++ React/central-student-hub/src/UserSessionComponent/Login.tsx
@@ -1,6 +1,6 @@
 import React, { FormEvent, MutableRefObject, useRef, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 import './Login.css';
 import { useNavigate } from "react-router-dom";
 // import { APIRequester } from "../Services/APIRequester.ts";
@@ -26,39 +26,36 @@ export default function Login() {
   }
 
   return (
-    <div id="login-container">
+    <div className="login-container" id="login-container">
       <img 
         src="https://cdn.discordapp.com/attachments/1169703919698587750/1178702979403100230/vik2l5e9.png?ex=65771bb1&is=6564a6b1&hm=06edc38d3e73db2f12fc0980001056c28f98fcd6e9578624b5411292bf7627a6&"
         alt="Central Student Hub logo"
       />
-      
-      
+
       <form className="login-form" action="submit" onSubmit={(e) => onLogin(e)}>
         
         <h1 className="login-label">login</h1>
         
-        <div className="email-input">
-          <input ref={emailInput} type="email" id="email" placeholder="email" />
+        <div className="input-container email-container" email-icon='&#128232;'>
+          <input className="email" ref={emailInput} type="email" id="email" placeholder=" email" />
+        </div>
+        
+        <div className="input-container password-container" password-icon='&#x1F510;'>
+          <input className="password" ref={passwordInput} type="password" id="password" placeholder="password" />
         </div>
 
-        <div className="password-input">
-          <input ref={passwordInput} type="password" id="password" placeholder="password" />
-        </div>
-
-        {invalid ? <p className="error">Invalid email or password</p> : <></>}
+        {invalid ? <p className="error">invalid email or password</p> : <></>}
         
         <a className="forget-pass" href="http://localhost:3000/forgot-my-password">forgot password?</a>
 
         <button className="login-button" type="submit">login</button>
 
-        <div className='google-icon'>
+        <div className='OAuth'>
           <span>login with</span>
           <FontAwesomeIcon icon={faGoogle} />
         </div>
 
-        <div className="signUp-button">
-          <button onClick={() => navigate("/signup")}>sign up</button>
-        </div>
+        <button className="signUp-button" onClick={() => navigate("/signup")}>sign up</button>
 
       </form>
     </div>
