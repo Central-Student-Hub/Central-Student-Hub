194d3a842bd576553ad4a0baeaa8a7afc208cba6
Ali Mo'nes <AliIsHomie@outlook.com>
2023-11-29 00:42:10 +0200

    Add signup api request method

--- React/central-student-hub/src/UserSessionComponent/Models/SignupModels.ts
+++ React/central-student-hub/src/UserSessionComponent/Models/SignupModels.ts
@@ -1,7 +1,13 @@
+export type UserType = "Student" | "Teacher" | "Admin"
+
 export type SignupRequest = {
-    
+    ssn: string
+    email: string,
+    password: string,
+    userType: UserType
 }
 
 export type SignupResponse = {
-    
+    message: string,
+    accept: boolean
 }
--- React/central-student-hub/src/services/APIRequester.ts
+++ React/central-student-hub/src/services/APIRequester.ts
@@ -1,11 +1,15 @@
 import { LoginResponse, LoginRequest } from '../UserSessionComponent/Models/LoginModels.ts'
 import { SignupResponse, SignupRequest } from '../UserSessionComponent/Models/SignupModels.ts'
 
+const headers: HeadersInit = { "Content-Type": "application/json" };
+const requestOptions = { mode: 'cors', method: "post", headers: headers }
+
 export class APIRequester {
     async login(request: LoginRequest): Promise<boolean> {
         try {
             const headers: HeadersInit = { "Content-Type": "application/json" };
-            const response: Response = await fetch("http://localhost:8082/auth/login", { mode: 'cors', body: JSON.stringify(request), method: "post", headers: headers });
+            const requestOptions: RequestInit = { mode: 'cors', body: JSON.stringify(request), method: "post", headers: headers };
+            const response: Response = await fetch("http://localhost:8082/auth/login", requestOptions);
             const loginResponse: LoginResponse = await response.json();
             if (!loginResponse.accept)
                 return false;
@@ -16,15 +20,19 @@ export class APIRequester {
             return false;
         }
     }
+
+    async signup(request: SignupRequest): Promise<SignupResponse> {
+        try {
+            const headers: HeadersInit = { "Content-Type": "application/json" };
+            const requestOptions: RequestInit = { mode: 'cors', body: JSON.stringify(request), method: "post", headers: headers };
+            const response: Response = await fetch("http://localhost:8082/auth/signUp", requestOptions);
+            return await response.json();
+        } catch (error) {
+            console.error(error);
+            return { 
+                message: "Network Error!",
+                accept: false
+            };
+        }
+    }
 }
-
-
-// async function signup(request: LoginRequest): Promise<boolean> {
-//     try {
-//         const response: Response = await fetch("http://localhost:8080/login", { mode: 'cors', body: JSON.stringify(request) });
-//         return await response.json();
-//     } catch (error) {
-//         console.error(error);
-//         return false;
-//     }
-// }
