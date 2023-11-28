77b00103c6bfe8bb643a84f45fb6eb3789d3a56d
Ali Mo'nes <AliIsHomie@outlook.com>
2023-11-29 00:19:43 +0200

    fixed login request path

--- React/central-student-hub/src/services/APIRequester.ts
+++ React/central-student-hub/src/services/APIRequester.ts
@@ -5,7 +5,7 @@ export class APIRequester {
     async login(request: LoginRequest): Promise<boolean> {
         try {
             const headers: HeadersInit = { "Content-Type": "application/json" };
-            const response: Response = await fetch("http://localhost:8080/login", { mode: 'cors', body: JSON.stringify(request), method: "post", headers: headers });
+            const response: Response = await fetch("http://localhost:8082/auth/login", { mode: 'cors', body: JSON.stringify(request), method: "post", headers: headers });
             const loginResponse: LoginResponse = await response.json();
             if (!loginResponse.accept)
                 return false;
