b23f99506f2c99482e229639d75c74ec9934f7f8
Ziad Reda <engziadreda@gmail.com>
2023-12-07 19:08:21 +0200

    Creating user profile tables done.

--- Database/database-creation.sql
+++ Database/database-creation.sql
@@ -13,3 +13,42 @@ create table `userAccount` (
 	primary key (`userAccountId`, `ssn`),
     unique key (`ssn`)
 ) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_0900_ai_ci;
+
+CREATE TABLE officeHours (
+    id INT PRIMARY KEY,
+    fromTime INT,
+    toTime INT,
+    weekDay VARCHAR(20),
+    location VARCHAR(255)
+);
+
+CREATE TABLE TeachingStaffProfileInfo (
+    teacherId INT PRIMARY KEY,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    biography VARCHAR(255),
+    profilePictureURL VARCHAR(255),
+    department VARCHAR(255)
+);
+
+CREATE TABLE StudentProfileInfo (
+    studentId INT PRIMARY KEY,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    biography TEXT,
+    profilePictureURL VARCHAR(255),
+    major VARCHAR(255),
+    minor VARCHAR(255),
+    level INT NOT NULL,
+    noOfHours INT DEFAULT 0,
+    GPA FLOAT
+);
+
+CREATE TABLE Warning (
+    warningId INT PRIMARY KEY,
+    studentId INT,
+    reason VARCHAR(255),
+    date DATE,
+    FOREIGN KEY (studentId) REFERENCES StudentProfileInfo(studentId)
+);
+
