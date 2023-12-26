2c22a6c5f80c8177f26d84c4662e459afc07c8d3
Ziad Reda <engziadreda@gmail.com>
2023-12-26 17:55:01 +0200

    ExamService is created

--- Springboot/src/main/java/com/centralstudenthub/service/ExamService.java
+++ Springboot/src/main/java/com/centralstudenthub/service/ExamService.java
@@ -1,11 +1,18 @@
 package com.centralstudenthub.service;
 
+import com.centralstudenthub.entity.exam.Exam;
 import com.centralstudenthub.repository.ExamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ExamService {
     @Autowired
     private ExamRepository examRepository;
+
+    public List<Exam> getExamsList(Integer id) {
+        return examRepository.findAllByStudentId(id);
+    }
 }
