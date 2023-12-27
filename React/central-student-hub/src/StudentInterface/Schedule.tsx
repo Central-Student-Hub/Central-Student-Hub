908a9105d327b454642b1d06a4b43cc21a223985
mostafa336 <96492072+mostafa336@users.noreply.github.com>
2023-12-27 23:17:09 +0200

    exams {student ,admin} & Schedule

--- React/central-student-hub/src/StudentInterface/Schedule.tsx
+++ React/central-student-hub/src/StudentInterface/Schedule.tsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Schedule.css';
+
+type SessionType = string;
+type Location = string; 
+
+type ScheduleEntry = {
+    courseName: string,
+    period: number,
+    weekday: string,
+    sessionType: SessionType,
+    teacherName: string,
+    location: Location
+};
 
 const Schedule: React.FC = () => {
-  return <div>Schedule Page</div>;
+  const [schedule, setSchedule] = useState<ScheduleEntry[]>([]);
+
+  const mockScheduleData = [
+    {
+      courseName: 'Mathematics 101',
+      period: 1,
+      weekday: 'Monday',
+      sessionType: 'Lecture',
+      teacherName: 'John Doe',
+      location: 'Room 101'
+    },
+    {
+      courseName: 'Physics 201',
+      period: 2,
+      weekday: 'Tuesday',
+      sessionType: 'Lab',
+      teacherName: 'Jane Smith',
+      location: 'Lab 202'
+    },
+  ];
+
+  useEffect(() => {
+    setSchedule(mockScheduleData);
+  }, []);
+
+  return (
+    <div className="schedule-table">
+      <div className="schedule-header">
+        <div>Course Name</div>
+        <div>Period</div>
+        <div>Weekday</div>
+        <div>Session Type</div>
+        <div>Teacher Name</div>
+        <div>Location</div>
+      </div>
+      {schedule.map((entry, index) => (
+        <div className="schedule-row" key={index}>
+          <div>{entry.courseName}</div>
+          <div>{entry.period}</div>
+          <div>{entry.weekday}</div>
+          <div>{entry.sessionType}</div>
+          <div>{entry.teacherName}</div>
+          <div>{entry.location}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Schedule;
